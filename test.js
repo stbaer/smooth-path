60e4a9dba66caff359d5eac8acc5fad72fb3fb37
Steffen Baer <stbaer.mail@gmail.com>
2015-08-05 18:08:15 +0800

    updated tape version

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var smoothPath = require('./')
-var test = require('tape').test
+var test = require('tape').test;
 
 // @TODO
 
@@ -10,8 +10,8 @@ test("smooth path tests", function(t) {
     console.log(smoothPath(path));
 
     t.equal(1, 0+1, 'has a test');
-    t.deepEqual( smoothPath(path), [ 5, 5, 15, 15, 18.75, 18.75, 16.25, 16.25 ],
-      'Readme example correct' );
+
+    t.deepEqual( smoothPath(path), [ 5, 5, 15, 15, 18.75, 18.75, 16.25, 16.25 ], 'Readme example correct' );
 
     t.end()
-})
+});
